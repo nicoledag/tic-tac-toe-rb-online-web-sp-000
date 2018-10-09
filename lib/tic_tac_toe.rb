036b7dd6ad8a51263ac588cab8146fab2e37c32e
Nicole D <nnardella@gmail.com>
2018-10-09 15:39:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,3 +32,19 @@ end
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+
+  user_input = gets.strip
+  index = input_to_index(user_input)
+
+  if valid_move?(board, index)
+    move(board, index, char)
+    display_board(board)
+
+  else
+    turn(board)
+
+ end
+end
