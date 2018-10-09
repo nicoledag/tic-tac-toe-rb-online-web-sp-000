219244a826be13bad31e2086eac2204b3542ca0a
Nicole D <nnardella@gmail.com>
2018-10-09 17:45:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,8 @@ def turn(board)
   index = input_to_index(user_input)
 
   if valid_move?(board, index)
-    move(board, index)
+    player = current_player(board)
+    move(board, index, player)
     display_board(board)
 
   else
