78a7fc738e900adb7c49ebc0d68187e834ea1f7b
Nicole D <nnardella@gmail.com>
2018-10-09 15:43:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,3 +48,17 @@ def turn(board)
 
  end
 end
+
+def turn_count(board)
+
+counter = 0
+
+board.each do |space|
+  if space == "X" || space == "O"
+  counter += 1
+
+ end
+end
+ return counter
+
+end
