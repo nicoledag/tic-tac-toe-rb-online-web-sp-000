9b6aaa7246e24c11de777635560fd21ae582a968
Nicole D <nnardella@gmail.com>
2018-10-09 16:08:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -123,7 +123,14 @@ def winner(board)
 end
 
 def play(board)
-
-  input = gets
-
+  until over?(board)
+    turn(board)
+  end
+  if winner(board) == "X"
+    puts "Congratulations X!"
+  elsif winner(board) == "O"
+    puts "Congratulations O!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
 end
